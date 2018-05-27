497dd3bd5a0a10d0629dda018c5be07d3ecd77aa
maaya <huki.j37@gmail.com>
2018-05-27 16:47:06 +0900

    no message

--- autocomplete/src/main/java/com/maaya/challenge/autocomplete/dao/DataStore.java
+++ autocomplete/src/main/java/com/maaya/challenge/autocomplete/dao/DataStore.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DataStore {
-    
+
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
     public List<String> createCandidates(String kind) {
