e7ecde44a35de3d7de90de7dc81862e5c7ba64c8
maaya <huki.j37@gmail.com>
2018-05-28 14:36:51 +0900

    no message

--- suggest/src/main/java/com/maaya/challenge/autocomplete/Controller.java
+++ suggest/src/main/java/com/maaya/challenge/autocomplete/Controller.java
@@ -1,18 +1,17 @@
 package com.maaya.challenge.autocomplete;
 
 import com.maaya.challenge.autocomplete.dao.DataStore;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 public class Controller {
 
+    @CrossOrigin(origins = "*")
     @RequestMapping(value = "/suggest", method = RequestMethod.GET)
     public List<String> createSuggest(@RequestParam String kind) {
+
         DataStore dataStore = new DataStore();
 
         return dataStore.createCandidates(kind);
